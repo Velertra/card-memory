919882772b2f15bc012f73adcd0e043ddf4b6e69
Velertra <75776124+Velertra@users.noreply.github.com>
2023-11-02 12:33:49 -0700

    added background and start page buttons

---
+++ src/components/Level.jsx
@@ -0,0 +1,12 @@
+const LevelChoices = () => {
+
+    return (
+        <>
+            <button>easy</button>
+            <button>mid</button>
+            <button>supes mid</button>
+        </>
+      );
+}
+ 
+export default LevelChoices;
---
+++ src/components/StartPage.jsx
@@ -0,0 +1,15 @@
+import LevelChoices from "./Level"
+
+const StatePage = () => {
+    
+    return (
+        <>  
+            <button>home</button>
+            <LevelChoices />
+            <button>milk</button>
+            <button>Mute</button>
+        </>
+      );
+}
+ 
+export default StatePage;
