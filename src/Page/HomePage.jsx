4b2e3a55301cd16836c20561ff07282ab786695b
Velertra <75776124+Velertra@users.noreply.github.com>
2024-01-09 22:21:50 -0700

    fixed image on dash

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -7,7 +7,7 @@ const HomePage = () => {
             <main className='home-page' id='home-page'>
                 <div className="app-container" id="app-container">Memory Game
                     <Link className="memory-link" id="memory-link" to="memory-game">
-                        <img src="/public/images/homeImages/cardmemroyss.png" className="memory-game-img" id="memory-game-img"></img>
+                        <img src="/images/homeImages/cardmemory.png" className="memory-game-img" id="memory-game-img"></img>
                     </Link>
                 </div>
             </main>
